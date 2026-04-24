92307adb36678e7ca086baf908982026c411cdcb
Wiesty <106901362+wiesty@users.noreply.github.com>
2026-04-25 00:51:52 +0200

    Remove 'allowedDevOrigins' from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
   serverExternalPackages: ['better-sqlite3'],
   output: 'standalone',
-  allowedDevOrigins: ['192.168.178.49'],
 }
 
 export default nextConfig
